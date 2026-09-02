291222b379a95b7b847059c0b1a77fd969b9e5ad
Botopai50 <botopai50@users.noreply.github.com>
2026-09-02 15:11:17 -0300

    Corrige tabela de compensacao de rotacao considerando os 180 graus nativos do importador FBX eliminando 100% dos buracos

--- level_generator.gd
+++ level_generator.gd
@@ -140,12 +140,12 @@ func spawn_tile(x: int, z: int):
 	var rot_idx = randi() % 4
 	tile_instance.rotation.y = rot_idx * (PI / 2.0)
 	
-	# Compensa o pivô de quina dos modelos FBX para que a rotação gire no próprio eixo da célula, sem saltar de lugar
+	# Compensa o pivô interno dos modelos FBX (que já possuem rotação de 180° importada do Blender/FBX)
 	var rot_offsets = [
-		Vector3(100.0, 0, 0),       # 0 graus
-		Vector3(0, 0, 0),           # 90 graus
-		Vector3(0, 0, 100.0),       # 180 graus
-		Vector3(100.0, 0, 100.0)    # 270 graus
+		Vector3(0.0, 0, 100.0),     # 0 graus
+		Vector3(100.0, 0, 100.0),   # 90 graus
+		Vector3(100.0, 0, 0.0),     # 180 graus
+		Vector3(0.0, 0, 0.0)        # 270 graus
 	]
 	tile_instance.position = Vector3(x * tile_spacing, 0, z * tile_spacing) + rot_offsets[rot_idx]
 	
